08368e3031ade749472d9dc6ae14424d42e9b6b2
muhsinking <muhsinking@gmail.com>
2015-07-29 00:12:10 -0500

    add READ endpoint to index.js

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,27 @@ var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index');
-// });
-//
-// module.exports = router;
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+module.exports = router;
 
 
 // CREATE
 router.post('/api/v1/todos', function(req, res) {
-
     var results = [];
 
     // Grab data from http request
     var data = {text: req.body.text, complete: false};
 
-    // Get a Postgres client from the connection pool
+    // Get a client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
 
-        // SQL Query > Insert Data
+        // Insert Data
         client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
 
-        // SQL Query > Select Data
+        // Select Data
         var query = client.query("SELECT * FROM items ORDER BY id ASC");
 
         // Stream results back one row at a time
@@ -45,4 +44,36 @@ router.post('/api/v1/todos', function(req, res) {
         }
 
     });
+});
+
+
+// READ
+router.get('/api/v1/todos/', function(req, res) {
+
+    var results = [];
+
+    // Get a client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC;");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            client.end();
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if(err) {
+          console.log(err);
+        }
+
+    });
+
 });
