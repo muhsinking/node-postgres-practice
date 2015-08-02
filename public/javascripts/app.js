f8390d89a4e75f4fe68d77fe60ab08f267b57c2c
muhsinking <muhsinking@gmail.com>
2015-08-02 15:48:06 -0500

    add post and delete to angular module

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -14,4 +14,25 @@ angular.module('nodeTodo', [])
         .error(function(error) {
             console.log('Error: ' + error);
         });
+
+    // Create a new todo
+    $http.post('/api/v1/todos', $scope.formData)
+        .success(function(data) {
+            $scope.formData = {};
+            $scope.todoData = data;
+            console.log(data);
+        })
+        .error(function(error) {
+            console.log('Error: ' + error);
+        });
+
+    // Delete a todo
+    $http.delete('/api/v1/todos/' + todoID)
+        .success(function(data) {
+            $scope.todoData = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
 });
