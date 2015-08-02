082ba9bad37143482108cf393211feb7dc3a05f1
muhsinking <muhsinking@gmail.com>
2015-08-02 15:17:58 -0500

    update index.jade

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
 
 module.exports = router;
 
