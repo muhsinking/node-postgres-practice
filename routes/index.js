a31f895574b97505497933daffdc23e8f3930169
muhsinking <muhsinking@gmail.com>
2015-08-02 16:02:01 -0500

    remove path dependency from index.js

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,13 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
-var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-
 module.exports = router;
-
 
 // CREATE
 router.post('/api/v1/todos', function(req, res) {
