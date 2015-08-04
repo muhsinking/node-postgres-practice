af9d524bda5e0cb81f1de92a62758280ea094c88
Mo King <muhsinking@gmail.com>
2015-08-03 20:51:46 -0500

    update delete in angular controller

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -26,13 +26,15 @@ angular.module('nodeTodo', [])
             console.log('Error: ' + error);
         });
 
-    // Delete a todo
-    $http.delete('/api/v1/todos/' + todoID)
-        .success(function(data) {
-            $scope.todoData = data;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
+        // Delete a todo
+    $scope.deleteTodo = function(todoID) {
+        $http.delete('/api/v1/todos/' + todoID)
+            .success(function(data) {
+                $scope.todoData = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
 });
